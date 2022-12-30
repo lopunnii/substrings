ae73b833bc4d352cb163bd1ea6e2731b98241b4f
Lopunnii <kimi7@xtra.co.nz>
2022-12-30 15:43:42 +0900

    Add dictionary array and empty substrings function

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it",
+              "i","low","own","part","partner","sit"]
+
+def substrings(words, dictionary)
+
+end
+
+substrings("below", dictionary)
